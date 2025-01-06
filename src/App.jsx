9146c362bb939a5be030372e065a7c87c08d9a5a
kdevante <144164265+kdevante@users.noreply.github.com>
2025-01-05 20:26:50 -0700

    Initialised repo and created a placeholder

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import funnyGif from "./assets/mrbean.gif";
+import stillhere from "./assets/stillhere.gif";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faInstagram,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+function App() {
+  let gif = funnyGif;
+  const [timer, setTimer] = useState(false)
+  useEffect(() => {
+    setTimeout(()=>{
+      setTimer(true);}, 20000);
+    }, []);
+  if (timer) {
+    gif = stillhere
+  }  
+  return (
+    <>
+      <div className="main">
+        <img className="img" src={gif} alt="Funny GIF" />
+        <h2>Back soon...</h2>
+        <nav className="links">
+          <a href="https://github.com/kdevante" target="_blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="mailto:kwizeradevante@gmail.com" target="_blank">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/devante-kwizera-6a55031a2/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://www.instagram.com/_.devante_/" target="_blank">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default App;
